7a650289b7cc58e3a1615dc685c9e7a27e2af231
GonzaloGuasch <gonzaloguasch@outlook.com>
2019-11-02 12:25:15 -0300

    BookPage que se ve bastante feo

--- src/api.jsx
+++ src/api.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const server = 'http://localhost:8080';
+const server = 'http://localhost:8080/';
 
 const request = (type, path, body) => axios
   .request({ url: `${server}${path}`, method: type, data: body })
   .then(req => req.data);
 
-export const findBookbyId = body => request('get', '/'+ body.id);
-export const books  = body => request('get', '/books');
-export const newBook = body => request('post', '/new', body);
-export const findBookByName =body => request('get', '/name/'+ body.name);
+export const findBookbyId = body => request('get', 'books/'+ body.id);
+export const books  = body => request('get', 'books/books');
+export const newBook = body => request('post', 'books/new', body);
+export const findBookByName =body => request('get', 'books/name/'+ body.name);
  
