9423ba4fa6f3ae71ca971281ff139b96c9229c83
fedecame <fede.cameriere@gmail.com>
2019-11-07 17:44:03 -0300

    Footer added, still missing 'AboutUs' page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,48 +2,45 @@ import React from 'react';
 import './css/Footer.css';
 
 class Footer extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
     render(){
       return(
-        <div className="footer">
-          <div className="grid-footer-container">
-              <div className="col-md-3 footer-left">
-                <h5>Direccion</h5>
-                <ul>
-                  <li>Buenos Aires Argentina</li>
-                  <li>Calle Falsa 1234</li>
-                  <li>Bs As. CP 666</li>
-                  <li>CUIT: 03 - 03456</li>
-                </ul>				
-              </div>
-              <div className="col-md-3 footer-left">
-                <h5>Blaaa</h5>
-                <ul>
-                  <li>Bla bla bla</li>
-                  <li>Bla bla bla</li>
-                  <li>Bla bla bla</li>
-                </ul>
-              </div>
-              <div className="col-md-3 footer-left">
-                <h5>Blaa Blaa</h5>
-                <ul>
-                  <li><a href="/us">Bla</a></li> 
-                  <li><a href="/contact">Bla</a></li>
-                </ul>
-              </div>
-              <div className="col-md-3 footer-right">
+        <React.Fragment>
+        <footer className="footer-distributed">
 
-                <p>© 2019 ar.edu.unq.eis Powered by <a href="https://github.com/ValenzuelaH/nombrePendienteII-MonoRepo">nombrePendieteII</a></p>
-                <p><div className="icons">
-                  <ul>
-                    <li><a href="https://twitter.com/?lang=es" className="twitter"> </a></li>
-                    <li><a href="https://web.facebook.com/MorfApp-490615881686201/?modal=admin_todo_tour" className="twitter facebook"> </a></li>
-                    <li><a href="https://www.google.com/search?client=ubuntu&channel=fs&q=morfapp&ie=utf-8&oe=utf-8" className="twitter chrome"> </a></li>
-                  </ul>
-                </div>
-                </p>
-              </div>
-           </div>
-           </div>
+          <div className="footer-right">
+    
+            <a href="https://www.facebook.com"><i className="fa fa-facebook"></i></a>
+            <a href="https://www.twitter.com"><i className="fa fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/"><i className="fa fa-linkedin"></i></a>
+            <a href="https://www.github.com/"><i className="fa fa-github"></i></a>
+    
+          </div>
+    
+          <div className="footer-left">
+    
+            <p className="footer-links">
+              <a className="link-1" href="">Inicio</a>
+    
+              {/* <a href="#">Blog</a>
+    
+              <a href="#">Pricing</a> */}
+    
+              <a href="">Quienes Somos</a>
+    
+              {/* <a href="#">Faq</a>
+    
+              <a href="#">Contact</a> */}
+            </p>
+    
+            <p>{this.props.title} &copy; 2019</p>
+          </div>
+  
+        </footer>
+        </React.Fragment>
       );
     }
 }
