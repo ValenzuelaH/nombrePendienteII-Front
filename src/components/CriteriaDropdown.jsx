f02f33e414b2c4340e5b255c89bb60b7f37d58a9
ValenzuelaH <horacio.valenzuela@alu.unq.edu.ar>
2019-11-09 21:06:28 -0300

    Adding last details in navbar no-logic

--- src/components/CriteriaDropdown.jsx
+++ src/components/CriteriaDropdown.jsx
@@ -1,43 +1,26 @@
 import React from 'react';
 
-var dropDownItems = [ {"code":"0", "name":"-- Seleccione un criterio --"},
+var dropDownItems = [ {"code":"0", "name":"Nombre (libro)", "route":"/contact"},
+                      {"code":"2", "name":"Nombre (Autor)"},
                       {"code":"1", "name":"ID"},
-                      {"code":"2", "name":"Nombre (libro)"},
-                      {"code":"3", "name":"Nombre (Autor)"},
-                      {"code":"4", "name":"Género"}
+                      {"code":"3", "name":"Género"}
                     ]
 
 class CriteriaDropdown extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state={
-            select: 0
-        }
-        this.select = this.select.bind(this)
-    }
-
-    select(index){
-        this.setState({select:index})
-    }
-
     render(){
         return(
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                     {dropDownItems.find(e => e.code == this.state.select).name}
+                    <i>-- Seleccione un criterio --</i> 
             </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <button class="dropdown-item" type="button" onClick={this.select(0)}>       <i>-- Seleccione un criterio --</i></button>
-                    <button class="dropdown-item" type="button" onClick={this.select(1)}>       ID</button>
-                    <button class="dropdown-item" type="button" onClick={this.select(2)}>       Nombre <i>(libro)</i></button>
-                    <button class="dropdown-item" type="button" onClick={this.select(3)}>       Nombre <i>(Autor)</i></button>
-                    <button class="dropdown-item" type="button" onClick={this.select(4)}>       Género</button>
+                    <button class="dropdown-item active" type="button">       Nombre <i>(libro)</i></button>
+                    <button class="dropdown-item" type="button">       Nombre <i>(Autor)</i></button>
+                    <button class="dropdown-item" type="button">       ID</button>
+                    <button class="dropdown-item" type="button">       Género</button>
                  </div>
             </div>
        )}
 }
 
 export default CriteriaDropdown;
-
-
